5110f9d654427e6d1855963ee52e22ce0407b577
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-18 15:10:27 +0100

    end

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Chiedi all’utente il suo nome,
+let nomeUtente = prompt('Ciao! Qual è il tuo nome?');
+console.log(nomeUtente);
+
+// chiedi il suo cognome,
+let cognomeUtente = prompt('Adesso scrivi il tuo cognome');
+console.log(cognomeUtente);
+
+// chiedi il suo colore preferito
+let colorePref = prompt('Abbiamo quasi finito! Qual è il tuo colore preferito?')
+console.log (colorePref);
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+const numero = 21;
+
+let password = `la tua password è ${nomeUtente}${cognomeUtente}${colorePref}${numero}`;
+document.getElementById('mio_id').innerHTML = password;
